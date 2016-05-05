1babecc41ba0277f9adecb88f4a206ef0109f106
Paul Hobbel <paulhobbel@hotmail.com>
2016-05-06 00:47:18 +0200

    Fixed a bug that left the last item out...

--- src/Helper/ArrayHelper.js
+++ src/Helper/ArrayHelper.js
@@ -3,7 +3,7 @@
 class ArrayHelper {
     reverse(array) {
         let tmp = [];
-        for (let i = array.length-1; i !== 0; i--) {
+        for (let i = array.length-1; i >= 0; i--) {
             tmp.push(array[i]);
         }
         return tmp;
