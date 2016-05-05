da1fedad846e0520901e7ac105d0946d95976090
Paul Hobbel <paulhobbel@hotmail.com>
2016-05-06 00:31:25 +0200

    nobody saw that typo...

--- src/Helper/ArrayHelper.js
+++ src/Helper/ArrayHelper.js
@@ -2,7 +2,7 @@
 
 class ArrayHelper {
     reverse(array) {
-        let tmp = [],
+        let tmp = [];
         for (let i = array.length-1; i !== 0; i--) {
             tmp.push(array[i]);
         }
